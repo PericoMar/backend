6a07bed910adb43462c3b08bcc93180e4ce41d43
Pedro Martinez Gonzalez <pericomarytb@gmail.com>
2024-05-13 23:45:20 +0200

    [Update] routes

--- index.js
+++ index.js
@@ -3,11 +3,19 @@
 const express = require('express');
 const app = express();
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200'); // Reemplaza con el origen de tu aplicación Angular
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
+
 // Importar las rutas
 const peliculasRoutes = require('./src/routes/peliculasRoutes');
-const sesionesRoutes = require('./src/routes/sesionesRoutes');
-const reservasRoutes = require('./src/routes/reservasRoutes');
-const usuariosRoutes = require('.src/router/usuariosRoutes');
+// const sesionesRoutes = require('./src/routes/sesionesRoutes');
+// const reservasRoutes = require('./src/routes/reservasRoutes');
+// const usuariosRoutes = require('.src/routes/usuariosRoutes');
 // Importa otras rutas según sea necesario
 
 // Registrar las rutas en la aplicación
