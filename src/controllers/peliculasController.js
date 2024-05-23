45c0b67a527c1e87fdd520c26fecdbb7431fb3ed
Pedro Martinez Gonzalez <pericomarytb@gmail.com>
2024-05-23 21:13:22 +0200

    [Update] Fix errors

--- src/controllers/peliculasController.js
+++ src/controllers/peliculasController.js
@@ -9,3 +9,17 @@ exports.obtenerTodas = async (req, res) => {
     res.status(500).json({ error: 'Error al obtener todas las películas' });
   }
 };
+
+exports.obtenerPorId = async (req, res) => {
+  try {
+    const id = req.params.id; // Obtener el id de los parámetros de la URL
+    const pelicula = await Pelicula.obtenerPeliculaPorId(id);
+    if (!pelicula) {
+      return res.status(404).json({ error: 'Película no encontrada' });
+    }
+    res.json(pelicula);
+  } catch (error) {
+    console.error('Error al obtener película por ID:', error);
+    res.status(500).json({ error: 'Error al obtener película por ID' });
+  }
+};
--- src/routes/peliculasRoutes.js
+++ src/routes/peliculasRoutes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const peliculasController = require('../controllers/peliculasController');
 
 router.get('/peliculas', peliculasController.obtenerTodas);
+router.get('/pelicula/:id', peliculasController.obtenerPorId);
 
 module.exports = router;
