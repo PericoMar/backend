f31afa982ab283e97618bc3196eaaa6b2a74e839
Pedro Martinez Gonzalez <pericomarytb@gmail.com>
2024-06-07 12:38:11 +0200

    [Delete] Files

--- src/routes/peliculasRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const peliculasController = require('../controllers/peliculasController');
-
-router.get('/peliculas', peliculasController.obtenerTodas);
-router.get('/pelicula/:id', peliculasController.obtenerPorId);
-
-module.exports = router;
